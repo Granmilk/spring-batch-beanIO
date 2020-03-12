9ed4e27f2a1219434763d7d183baa681f032014b
thalles.duarte <thalles.duarte@cabal.com.br>
2020-03-12 15:47:18 -0300

    first commit

---
+++ src/main/java/com/thalles/batchbeanio/BatchBeanioApplication.java
@@ -0,0 +1,13 @@
+package com.thalles.batchbeanio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchBeanioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchBeanioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thalles/batchbeanio/BatchBeanioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thalles.batchbeanio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchBeanioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
